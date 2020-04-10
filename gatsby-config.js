8c5633936fecacd6f65fa81d7f887fc1f6340d92
Dave Cox <davedvst@gmail.com>
2020-04-10 11:14:26 -0700

    removed contentful from gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,7 @@ module.exports = {
     author: "D.Cox",
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-    },
+
     "gatsby-plugin-sass",
     "gatsby-plugin-styled-components",
     {
